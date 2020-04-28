9fb26d2ef5058e486e2fd2152af9c63eb71fbe19
Eric Knutson <eric@vorakortech.io>
2020-04-27 20:40:52 -0700

    Getting rid of Interiori

--- app/baseDjangoDocker/asgi.py
+++ app/baseDjangoDocker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Interiori project.
+ASGI config for baseDjangoDocker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Interiori.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseDjangoDocker.settings')
 
 application = get_asgi_application()
--- app/baseDjangoDocker/wsgi.py
+++ app/baseDjangoDocker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Interiori project.
+WSGI config for baseDjangoDocker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Interiori.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseDjangoDocker.settings')
 
 application = get_wsgi_application()
